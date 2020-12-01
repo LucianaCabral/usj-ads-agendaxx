3f68ba0a03d222e7dd5e1c824a62dc3d127a7c84
Luciana <eng.luciana.cabral@gmail.com>
2020-11-30 21:25:13 -0300

    Clean code

--- src/main/java/br/edu/usj/ads/lgii/agendax/ContatoController.java
+++ src/main/java/br/edu/usj/ads/lgii/agendax/ContatoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class ContatoController {
    @Autowired 
    ContatoRepository contatoRepository;
+   
    @GetMapping(value="/") //raiz
    public ModelAndView getListaTodos() {//raiz
        List<Contato> lista = contatoRepository.findAll();//raiz
